4c3829b004e43551772a749c887ea3e12c1a34aa
Shaun Yap <shaun@siegeflow.com>
2019-02-19 11:10:14 -0800

    added confetti

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import { bubble as Menu } from 'react-burger-menu'
+import { bubble as Menu } from 'react-burger-menu';
+import { confetti } from 'dom-confetti';
 
 class Header extends Component {
   showSettings (event) {
     event.preventDefault();
   }
+  componentDidMount() {
+    const button = document.querySelector("#menu-logo");
+    const confettiConfig = {
+      angle: "0",
+      spread: 45,
+      startVelocity: 100,
+      elementCount: "150",
+      dragFriction: 0.1,
+      duration: 3000,
+      delay: 0,
+      width: "10px",
+      height: "10px",
+      colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+    };
+    button.addEventListener("click", () => confetti(button, confettiConfig));
+  }
   render() {
     return (
       <div>
         <header className="topbar">
-        <h3>siegeflow</h3>
+        <h3 id="menu-logo">siegeflow</h3>
         <Menu right>
           <a id="home" className="menu-item" href="/">Home</a>
           <a id="projects" className="menu-item" href="/projects">Projects</a>
