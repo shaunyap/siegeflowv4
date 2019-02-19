948532dcc7965e34749a657d22337adcf3c2f90d
Shaun Yap <shaun@siegeflow.com>
2019-02-19 10:56:13 -0800

    add hamburger menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,18 @@ import { graphql, Link } from "gatsby";
 import Layout from '../components/layout';
 
 export default ({ data }) => {
-  console.log(data);
   return (
           <Layout>
               <section className="hero flex-center">
                 <div className="intro">
                   <h1>Hello world!</h1>
-                  <p>I'm Shaun, and I'm looking for a career switch to React or iOS development. I've spent the last four years working on personal coding projects, and sometimes even maneuvering into my full-time gig.</p>
+                  <p>I'm Shaun, I've spent the last four years working on personal coding projects, and now I'm looking for a career switch to React or iOS development.</p>
 
                   <p>Previously, I've had an exciting career in digital marketing which has taken me everywhere from Singapore to Dubai, San Francisco to Taipei. I've even picked up some awards along the way.</p>
 
-                  <p>Despite that, I've always had a niggling feeling that my true passions lay elsewhere. My journey has taken me into the world of development. And after years of learning and experimenting, I'm ready to show I have what it takes to thrive.</p>
+                  <p>Despite that, I've always had a feeling that my true passions lay elsewhere. And after years of learning and experimenting, I'm ready to show I have what it takes to thrive.</p>
 
-                  <p> So let me buy you coffee. Better yet, let me show you on a whiteboard.</p>
+                  <p>So let me buy you coffee. Better yet, let me show you on a whiteboard.</p>
                 </div>
               </section>
               <section className="split-container flex-center-med-up">
