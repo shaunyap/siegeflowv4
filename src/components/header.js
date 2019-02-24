8ac045422635a987b5e68fff48d3edee7c8b2011
Shaun Yap <shaun@siegeflow.com>
2019-02-24 11:21:18 -0800

    add header link, moved confetti

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { bubble as Menu } from 'react-burger-menu';
-import { confetti } from 'dom-confetti';
-const confettiConfig = {
-  angle: "0",
-  spread: 45,
-  startVelocity: 100,
-  elementCount: "150",
-  dragFriction: 0.1,
-  duration: 3000,
-  delay: 0,
-  width: "10px",
-  height: "10px",
-  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
-};
 
-class Header extends Component {
-  showSettings (event) {
-    event.preventDefault();
-  }
-  componentDidMount() {
-    const button = document.querySelector("#menu-logo");
-    button.addEventListener("click", () => confetti(button, confettiConfig));
-  }
-  render() {
-    return (
-      <div>
-        <header className="topbar">
-        <h3 id="menu-logo">siegeflow</h3>
-        <Menu right>
-          <a id="home" className="menu-item" href="/">Home</a>
-          <a id="projects" className="menu-item" href="/projects">Projects</a>
-          <a id="blog" className="menu-item" href="/blog">Blog</a>
-          <a id="contact" className="menu-item" href="/contact">Contact</a>
-        </Menu>
-        </header>
-      </div>
-    )
-  }
+export default Header => {
+  return (
+    <div>
+      <header className="topbar">
+      <h3 id="menu-logo"><a href="/">siegeflow</a></h3>
+      <Menu right>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="projects" className="menu-item" href="/projects">Projects</a>
+        <a id="blog" className="menu-item" href="/blog">Blog</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+      </Menu>
+      </header>
+    </div>
+  )
 }
-
-export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,93 @@
-import React from "react";
+import React, { Component } from "react";
 import { graphql, Link } from "gatsby";
 import Layout from '../components/layout';
+import { confetti } from 'dom-confetti';
+const confettiConfig = {
+  angle: "75",
+  spread: 100,
+  startVelocity: 40,
+  elementCount: "150",
+  dragFriction: 0.1,
+  duration: 8000,
+  delay: 0,
+  width: "10px",
+  height: "10px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
 
-export default ({ data }) => {
-  return (
-          <Layout>
-              <section className="hero flex-center">
-                <div className="intro">
-                  <h1>Hello world!</h1>
-                  <p>I'm Shaun, and after spending the last four years working on <a href="/">personal coding projects</a>, I'm now looking for a career switch to React or iOS development.</p>
+class Index extends Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
 
-                  <p>Previously, I've had an exhilarating career in digital marketing which has taken me everywhere from Singapore to Dubai, San Francisco to Taipei. <span id="sma-hover">I've even picked up some awards along the way.</span></p>
+  componentDidMount() {
+    const button = document.querySelector("#helloWorld");
+    button.addEventListener("click", () => {
+      confetti(button, confettiConfig)
+    });
+  }
 
-                  <p>Despite that, I've always had a feeling that my true passions lay elsewhere. And after years of tinkering and experimenting, I'm ready to show I have what it takes to thrive.</p>
+  render() {
+    const { data } = this.props;
+    return (
+            <Layout>
+                <section className="hero flex-center">
+                  <div className="intro">
+                    <h1 id="helloWorld">Hello world!</h1>
+                    <p>I'm Shaun, and after spending the last four years working on <a href="/">personal coding projects</a>, I'm now looking for a career switch to React or iOS development.</p>
 
-                  <p>So let me buy you coffee. Better yet, let me show you on a whiteboard.</p>
-                </div>
-              </section>
-              <section className="split-container flex-center-med-up">
-                <div className="left-container">
-                  <div className="lefty flex-center v-center">
-                    <div>
-                        <h2>Projects</h2>
-                        <ul>
-                        {data.allMarkdownRemark.edges
-                          .filter(({node}) => node.frontmatter.category==="project") 
-                          .map( ({ node }, index) => 
-                          (
-                            <li key={index}>
-                              <Link to = {node.frontmatter.path}>
-                                {node.frontmatter.title}
-                              </Link>
-                            </li>
-                          )
-                        )}
-                        </ul>
+                    <p>Previously, I've had an exhilarating career in digital marketing which has taken me everywhere from Singapore to Dubai, San Francisco to Taipei. <span id="sma-hover">I've even picked up some awards along the way.</span></p>
+
+                    <p>Despite that, I've always had a feeling that my true passions lay elsewhere. And after years of tinkering and experimenting, I'm ready to show I have what it takes to thrive.</p>
+
+                    <p>So let me buy you coffee. Better yet, let me show you on a whiteboard.</p>
+                  </div>
+                </section>
+                <section className="split-container flex-center-med-up">
+                  <div className="left-container">
+                    <div className="lefty flex-center v-center">
+                      <div>
+                          <h2>Projects</h2>
+                          <ul>
+                          {data.allMarkdownRemark.edges
+                            .filter(({node}) => node.frontmatter.category==="project") 
+                            .map( ({ node }, index) => 
+                            (
+                              <li key={index}>
+                                <Link to = {node.frontmatter.path}>
+                                  {node.frontmatter.title}
+                                </Link>
+                              </li>
+                            )
+                          )}
+                          </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="right-container">
-                  <div className="righty flex-center v-center">
-                    <div>
-                        <h2>Blog</h2>
-                        <ul>
-                        {data.allMarkdownRemark.edges
-                          .filter(({node}) => node.frontmatter.category==="blog") 
-                          .map( ({ node }, index) => 
-                          (
-                            <li key={index}>
-                              <Link to = {node.frontmatter.path}>
-                                {node.frontmatter.title}
-                              </Link>
-                            </li>
-                          )
-                        )}
-                        </ul>
+                  <div className="right-container">
+                    <div className="righty flex-center v-center">
+                      <div>
+                          <h2>Blog</h2>
+                          <ul>
+                          {data.allMarkdownRemark.edges
+                            .filter(({node}) => node.frontmatter.category==="blog") 
+                            .map( ({ node }, index) => 
+                            (
+                              <li key={index}>
+                                <Link to = {node.frontmatter.path}>
+                                  {node.frontmatter.title}
+                                </Link>
+                              </li>
+                            )
+                          )}
+                          </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </section>
-          </Layout>
-  )
+                </section>
+            </Layout>
+    )
+  }
 }
 
 export const query = graphql`
@@ -82,3 +109,5 @@ query getMarkdownPosts {
     }
 }
 `
+
+export default Index;
