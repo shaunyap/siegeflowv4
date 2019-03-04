a0917181c6adbc2080aca4171d21e77449ebae81
Shaun Yap <shaun@siegeflow.com>
2019-03-04 14:56:55 -0800

    bug fixes to projects layout

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,23 +5,25 @@ import { graphql } from 'gatsby';
 const Project = ({ data }) => {
     return (
         <Layout title="Projects: Front-end Developer based in Seattle | React.js | Swift">
-            <h1>Projects</h1>
-            <div className="flexgrid">
-            {data.allMarkdownRemark.edges
-                .filter(({node}) => node.frontmatter.category==="project")
-                .map( ({ node }, index) =>
-                (
-                    <div className="card col" key={index}>
-                        <a href={node.frontmatter.path}><img src={node.frontmatter.thumbnail.childImageSharp.fluid.src} alt="placeholder" /></a>
-                        <div className="project-meta">
-                            <h3><a href={node.frontmatter.path}>{node.frontmatter.title}</a></h3>
-                            <p className="subtext">
-                                {node.frontmatter.tags.join(", ")}
-                            </p>
-                        </div>
-                    </div>
-                )
-            )}
+            <div className="projects-container">
+              <h1>Projects</h1>
+              <div className="flexgrid">
+              {data.allMarkdownRemark.edges
+                  .filter(({node}) => node.frontmatter.category==="project")
+                  .map( ({ node }, index) =>
+                  (
+                      <div className="card col" key={index}>
+                          <a href={node.frontmatter.path}><img src={node.frontmatter.thumbnail.childImageSharp.fluid.src} alt="placeholder" /></a>
+                          <div className="project-meta">
+                              <h3><a href={node.frontmatter.path}>{node.frontmatter.title}</a></h3>
+                              <p className="subtext">
+                                  {node.frontmatter.tags.join(", ")}
+                              </p>
+                          </div>
+                      </div>
+                  )
+              )}
+              </div>
             </div>
         </Layout>
     );
