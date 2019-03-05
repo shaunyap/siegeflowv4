85ff9788c8068ab70fb0738cd7751fe2b9f45c44
Shaun Yap <shaun@siegeflow.com>
2019-03-05 10:21:11 -0800

    add Google Analytics Tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,20 @@ module.exports = {
             windows: false
           }
         }
+      },
+      {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-74688273-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
       }
+    }
     ]
 }
